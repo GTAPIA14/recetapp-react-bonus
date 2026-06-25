0938231e7b66827fe0c2160fe2ddb2c5c8bc32df
Gohans Tapia <gohans.tapia@inacapmail.cl>
2026-06-25 13:36:17 -0400

    feat: renderizado de la lista con key

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import RecetaCard from "./components/RecetaCard";
+import ListaRecetas from "./components/ListaRecetas";
 import { recetas } from "./data/recetas";
 
 function App() {
   return (
     <>
-      <h1>RecetApp</h1>
+      <h1
+        style={{
+          textAlign: "center",
+        }}
+      >
+        RecetApp
+      </h1>
 
-      <RecetaCard
-        nombre={recetas[0].nombre}
-        origen={recetas[0].origen}
-        porciones={recetas[0].porciones}
-        categoria={recetas[0].categoria}
-        descripcion={recetas[0].descripcion}
-        ingredientes={recetas[0].ingredientes}
-      />
+      <ListaRecetas recetas={recetas} />
     </>
   );
 }
---
+++ src/components/ListaRecetas.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+import RecetaCard from "./RecetaCard";
+
+function ListaRecetas({ recetas }) {
+  return (
+    <>
+      {recetas.map((receta) => (
+        <RecetaCard
+          key={receta.id}
+          nombre={receta.nombre}
+          origen={receta.origen}
+          porciones={receta.porciones}
+          categoria={receta.categoria}
+          descripcion={receta.descripcion}
+          ingredientes={receta.ingredientes}
+        />
+      ))}
+    </>
+  );
+}
+
+ListaRecetas.propTypes = {
+  recetas: PropTypes.array.isRequired,
+};
+
+export default ListaRecetas;
